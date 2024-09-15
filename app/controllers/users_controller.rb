eec1299cd0b44c447849bd03b05f5ecc051c3669
EC2 Default User <ec2-user@ip-172-31-34-244.ap-northeast-1.compute.internal>
2024-09-15 12:39:18 +0000

    Bookers2[day1]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_uuser, olny: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+     @books = @user.books.page(params[:page])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @book = book.new
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user =User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 end
